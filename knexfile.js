bef293a0e2cebac13e988a5ed513d8354ff14011
rifkegribenes <rifkegribenes@gmail.com>
2018-12-25 16:25:57 -0800

    knex migration worked

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,44 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    development: {
-        client: 'pg',
-        connection: {
-            host     : '127.0.0.1',
-            user     : process.env.DATABASE_USER,
-            password : process.env.DATABASE_PASSWORD,
-            database : process.env.DATABASE_NAME
-        },
-        migrations: {
-            directory: './db/migrations'
-        },
-        seeds: {
-            directory: './db/seeds'
-        }
+  development: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME
     },
-    testing: {
-        client: 'pg',
-        connection: {
-            host     : '127.0.0.1',
-            user     : process.env.DATABASE_USER,
-            password : process.env.DATABASE_PASSWORD,
-            database : process.env.TEST_DATABASE_NAME
-        },
-        migrations: {
-            directory: './db/migrations'
-        },
-        seeds: {
-            directory: './db/seeds/testing'
-        }
+    migrations: {
+      directory: "./db/migrations"
     },
-    production: {
-        client     : 'pg',
-        connection : process.env.DATABASE_URL,
-        migrations : {
-            directory: './db/migrations'
-        },
-        seeds: {
-            directory: './db/seeds/production'
-        }
+    seeds: {
+      directory: "./db/seeds"
     }
+  },
+  testing: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.TEST_DATABASE_NAME
+    },
+    migrations: {
+      directory: "./db/migrations/testing"
+    },
+    seeds: {
+      directory: "./db/seeds/testing"
+    }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./db/migrations/production"
+    },
+    seeds: {
+      directory: "./db/seeds/production"
+    }
+  }
 };
