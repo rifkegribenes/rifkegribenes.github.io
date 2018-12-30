3759d25f50d382cd96d115dadfe959241736ce66
rifkegribenes <rifkegribenes@gmail.com>
2018-12-29 16:38:02 -0800

    magenta start/end circles

--- client/src/App.js
+++ client/src/App.js
@@ -128,23 +128,61 @@ class App extends Component {
     smoothscroll.polyfill();
 
     // add glow classes to rainbow stripes
-    setTimeout(() => {
-      const colors = [
-        "red",
-        "orange",
-        "yellow",
-        "green",
-        "blue",
-        "purple",
-        "magenta"
-      ];
-      colors.map(color => {
-        document
-          .getElementById(color)
-          .classList.add(`Background-glow--${color}`);
-        return null;
-      });
-    }, 1500);
+    // setTimeout(() => {
+    //   const colors = [
+    //     "red",
+    //     "orange",
+    //     "yellow",
+    //     "green",
+    //     "blue",
+    //     "purple",
+    //     "magenta"
+    //   ];
+    //   colors.map(color => {
+    //     document
+    //       .getElementById(color)
+    //       .classList.add(`Background-glow--${color}`);
+    //     return null;
+    //   });
+    // }, 1500);
+
+    //  document.addEventListener('DOMContentLoaded', function() {
+    //    // Select the node that will be observed for mutations
+    //    var targetNode = document.getElementById('target-node');
+    //    console.log(targetNode);
+
+    //    // Options for the observer (which mutations to observe)
+    //    var config = { childList: true };
+
+    //    // Callback function to execute when mutations are observed
+    //    var callback = function(mutationsList, observer) {
+    //        for(var mutation of mutationsList) {
+    //            if (mutation.type === 'childList') {
+    //                console.log('A child node has been added or removed.');
+    //            }
+    //        }
+    //    };
+
+    //    // Create an observer instance linked to the callback function
+    //    var observer = new MutationObserver(callback);
+
+    //    // Start observing the target node for configured mutations
+    //    observer.observe(targetNode, config);
+
+    //    // Later, you can stop observing
+    //    // observer.disconnect();
+    // });
+
+    // hide css stripes, display raster rainbow
+    // document.addEventListener('DOMContentLoaded', function() {
+    //   console.log(document
+    //       .getElementById("raster"));
+
+    //     document
+    //       .getElementById("raster")
+    //       .classList.add('Raster-rainbow-end');
+
+    // });
 
     // If not logged in, check local storage for authToken
     // if it doesn't exist, it returns the string "undefined"
--- client/src/components/Rainbow.jsx
+++ client/src/components/Rainbow.jsx
@@ -3,30 +3,84 @@
 
 import React from "react";
 import PropTypes from "prop-types";
+import RasterRainbow from "../img/neonrainbow_transp-660.png";
 
 const Rainbow = props => (
-  <div className="Rainbow">
-    <span className="Rainbow-stripe Rainbow-stripe--magenta" />
-    <span className="Rainbow-stripe Rainbow-stripe--purple" />
-    <span className="Rainbow-stripe Rainbow-stripe--blue" />
-    <span className="Rainbow-stripe Rainbow-stripe--green" />
-    <span className="Rainbow-stripe Rainbow-stripe--yellow" />
-    <span className="Rainbow-stripe Rainbow-stripe--orange" />
-    <span className="Rainbow-stripe Rainbow-stripe--red" />
-    <span className="Rainbow-stripe Rainbow-stripe--magenta-light" />
-    <span className="Rainbow-stripe Rainbow-stripe--purple-light" />
-    <span className="Rainbow-stripe Rainbow-stripe--blue-light" />
-    <span className="Rainbow-stripe Rainbow-stripe--green-light" />
-    <span className="Rainbow-stripe Rainbow-stripe--yellow-light" />
-    <span className="Rainbow-stripe Rainbow-stripe--orange-light" />
-    <span className="Rainbow-stripe Rainbow-stripe--red-light" />
-    <span id="magenta" />
-    <span id="purple" />
-    <span id="blue" />
-    <span id="green" />
-    <span id="yellow" />
-    <span id="orange" />
-    <span id="red" />
+  <div className="dblWrap">
+    {/*    <div className="Raster-rainbow-start target-img" id="raster">
+      <img src={RasterRainbow}
+        alt="rainbow"
+        className="raster-img"
+      />
+    </div>*/}
+    <div className="Glow-containers">
+      <span className="Glow-container--start Glow-container--magenta">
+        <span className="Glow-start Glow-start--magenta" />
+      </span>
+      <span className="Glow-container Glow-container--magenta">
+        <span className="Glow-end Glow-end--magenta" />
+      </span>
+      <span className="Glow-container--start Glow-container--purple">
+        <span className="Glow-start Glow-start--purple" />
+      </span>
+      <span className="Glow-container Glow-container--purple">
+        <span className="Glow-end Glow-end--purple" />
+      </span>
+      <span className="Glow-container--start Glow-container--blue">
+        <span className="Glow-start Glow-start--blue" />
+      </span>
+      <span className="Glow-container Glow-container--blue">
+        <span className="Glow-end Glow-end--blue" />
+      </span>
+      <span className="Glow-container--start Glow-container--green">
+        <span className="Glow-start Glow-start--green" />
+      </span>
+      <span className="Glow-container Glow-container--green">
+        <span className="Glow-end Glow-end--green" />
+      </span>
+      <span className="Glow-container--start Glow-container--yellow">
+        <span className="Glow-start Glow-start--yellow" />
+      </span>
+      <span className="Glow-container Glow-container--yellow">
+        <span className="Glow-end Glow-end--yellow" />
+      </span>
+      <span className="Glow-container--start Glow-container--orange">
+        <span className="Glow-start Glow-start--orange" />
+      </span>
+      <span className="Glow-container Glow-container--orange">
+        <span className="Glow-end Glow-end--orange" />
+      </span>
+      <span className="Glow-container--start Glow-container--red">
+        <span className="Glow-start Glow-start--red" />
+      </span>
+      <span className="Glow-container Glow-container--red">
+        <span className="Glow-end Glow-end--red" />
+      </span>
+    </div>
+    <div className="Rainbow hide" id="target-node">
+      <span className="Rainbow-stripe Rainbow-stripe--magenta" />
+      <span className="Rainbow-stripe Rainbow-stripe--purple" />
+      <span className="Rainbow-stripe Rainbow-stripe--blue" />
+      <span className="Rainbow-stripe Rainbow-stripe--green" />
+      <span className="Rainbow-stripe Rainbow-stripe--yellow" />
+      <span className="Rainbow-stripe Rainbow-stripe--orange" />
+      <span className="Rainbow-stripe Rainbow-stripe--red" />
+      */}
+      <span className="Rainbow-stripe Rainbow-stripe--magenta-light" />
+      <span className="Rainbow-stripe Rainbow-stripe--purple-light" />
+      <span className="Rainbow-stripe Rainbow-stripe--blue-light" />
+      <span className="Rainbow-stripe Rainbow-stripe--green-light" />
+      <span className="Rainbow-stripe Rainbow-stripe--yellow-light" />
+      <span className="Rainbow-stripe Rainbow-stripe--orange-light" />
+      <span className="Rainbow-stripe Rainbow-stripe--red-light" />
+      <span id="magenta" className="hide" />
+      <span id="purple" className="hide" />
+      <span id="blue" className="hide" />
+      <span id="green" className="hide" />
+      <span id="yellow" className="hide" />
+      <span id="orange" className="hide" />
+      <span id="red" className="hide" />
+    </div>
   </div>
 );
 
