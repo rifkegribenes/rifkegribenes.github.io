2eb99c0cb31b36b0de164123568b225db8d602e7
rifkegribenes <rifkegribenes@gmail.com>
2018-12-29 22:11:21 -0800

    start circle rotate values

--- client/src/components/Rainbow.jsx
+++ client/src/components/Rainbow.jsx
@@ -14,43 +14,43 @@ const Rainbow = props => (
       />
     </div>*/}
     <div className="Glow-containers">
-      <span className="Glow-container--start Glow-container--magenta">
+      <span className="Glow-container--start Glow-container-start--magenta">
         <span className="Glow-start Glow-start--magenta" />
       </span>
       <span className="Glow-container Glow-container--magenta">
         <span className="Glow-end Glow-end--magenta" />
       </span>
-      <span className="Glow-container--start Glow-container--purple">
+      <span className="Glow-container--start Glow-container-start--purple">
         <span className="Glow-start Glow-start--purple" />
       </span>
       <span className="Glow-container Glow-container--purple">
         <span className="Glow-end Glow-end--purple" />
       </span>
-      <span className="Glow-container--start Glow-container--blue">
+      <span className="Glow-container--start Glow-container-start--blue">
         <span className="Glow-start Glow-start--blue" />
       </span>
       <span className="Glow-container Glow-container--blue">
         <span className="Glow-end Glow-end--blue" />
       </span>
-      <span className="Glow-container--start Glow-container--green">
+      <span className="Glow-container--start Glow-container-start--green">
         <span className="Glow-start Glow-start--green" />
       </span>
       <span className="Glow-container Glow-container--green">
         <span className="Glow-end Glow-end--green" />
       </span>
-      <span className="Glow-container--start Glow-container--yellow">
+      <span className="Glow-container--start Glow-container-start--yellow">
         <span className="Glow-start Glow-start--yellow" />
       </span>
       <span className="Glow-container Glow-container--yellow">
         <span className="Glow-end Glow-end--yellow" />
       </span>
-      <span className="Glow-container--start Glow-container--orange">
+      <span className="Glow-container--start Glow-container-start--orange">
         <span className="Glow-start Glow-start--orange" />
       </span>
       <span className="Glow-container Glow-container--orange">
         <span className="Glow-end Glow-end--orange" />
       </span>
-      <span className="Glow-container--start Glow-container--red">
+      <span className="Glow-container--start Glow-container-start--red">
         <span className="Glow-start Glow-start--red" />
       </span>
       <span className="Glow-container Glow-container--red">
