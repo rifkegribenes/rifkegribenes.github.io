f8a9c5ca636dd564b45882d7c80cfb283527ca07
rifkegribenes <rifkegribenes@gmail.com>
2019-05-15 22:37:58 -0700

    fix lint erro

--- client/__tests__/components/Project.spec.js
+++ client/__tests__/components/Project.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import toJson from "enzyme-to-json";
+// import toJson from "enzyme-to-json";
 // import configureStore from 'redux-mock-store'; // Smart components
 
 // Component to be tested
